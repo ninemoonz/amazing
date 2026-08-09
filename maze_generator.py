16405e50c9d6235f0f0dbf87df0e63f8c804dead
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2026-08-09 18:34:44 +0200

    grid with MazeCell objs

--- maze_generator.py
+++ maze_generator.py
@@ -6,28 +6,46 @@ class MazeCell:
     def __init__(self, coordinates: tuple[int, int]) -> None:
         self.coordinates = coordinates
         self.visited: bool = False
+        self.north_wall: int = 1
+        self.east_wall: int = 2
+        self.south_wall: int = 4
+        self.west_wall: int = 8
+        self.cell_value: int = (self.north_wall + self.east_wall
+                                + self.south_wall + self.west_wall)
+        self.north_neighbor: "MazeCell | None" = None
+        self.east_neighbor: "MazeCell | None" = None
+        self.south_neighbor: "MazeCell | None" = None
+        self.west_neighbor: "MazeCell | None" = None
+
+    def show_status(self) -> None:
+        print(f"Coordination: {self.coordinates}")
+        print(f"Visit status: {self.visited}")
+        print(f"Value of this cell: {self.cell_value}")
 
 
 class MazeGen:
     def __init__(self, width: int, height: int,
-                 entry_point: tuple[int, int], exit_point: tuple[int, int],
+                 entry_point: tuple[int, int],
+                 exit_point: tuple[int, int],
                  seed: int = 0) -> None:
         self.width = width
         self.height = height
         self.entry_point = entry_point
-        self.maze_list: list[list[int]] = []
+        self.exit_point = exit_point
+        self.maze_list: list[list[MazeCell]] = []
 
-    def make_maze(self) -> None:
-        for _ in range(self.height):
-            row_list: list[int] = []
-            for _ in range(self.width):
-                row_list.append(15)
+    def gen_grid(self) -> None:
+        for y in range(self.height):
+            row_list: list[MazeCell] = []
+            for x in range(self.width):
+                coordinate: tuple = (x, y)
+                row_list.append(MazeCell(coordinate))
             self.maze_list.append(row_list)
 
-    def print_maze(self) -> None:
+    def print_grid(self) -> None:
         for element in self.maze_list:
-            print(element)
-            print()
+            for status in element:
+                print(status.show_status())
 
 
 def generator(maze_info: MazeConfig) -> None:
@@ -36,5 +54,5 @@ def generator(maze_info: MazeConfig) -> None:
     entry_p: tuple[int, int] = maze_info.entry_point
     exit_p: tuple[int, int] = maze_info.exit_point
     new_maze: MazeGen = MazeGen(width, height, entry_p, exit_p)
-    new_maze.make_maze()
-    new_maze.print_maze()
+    new_maze.gen_grid()
+    new_maze.print_grid()
