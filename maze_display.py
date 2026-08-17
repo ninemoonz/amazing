ba748e1ff624e721850149ad1d8ea9f4cd3043c8
Ladook <76804681+ninemoonz@users.noreply.github.com>
2026-08-17 17:35:14 +0200

    new maze display

--- maze_display.py
+++ maze_display.py
@@ -1,5 +1,6 @@
 from validate_and_build import MazeConfig
 
+
 def read_maze(filename: str) -> list[list[int]]:
     converted_list: list[list[int]] = []
     with open(filename) as f:
@@ -20,9 +21,9 @@ def render_maze(grid: list[list[int]],
     height = len(grid)
     width = len(grid[0])
     canvas: list[list[str]] = []
-    wall = '■'
+    wall = '██'
     for _ in range(2 * height + 1):
-        row = [' '] * (2 * width + 1)
+        row = ['  '] * (2 * width + 1)
         canvas.append(row)
     for r in range(height):
         for c in range(width):
@@ -30,9 +31,9 @@ def render_maze(grid: list[list[int]],
             cx = 2 * c + 1
             cy = 2 * r + 1
             if r == entry_p[1] and c == entry_p[0]:
-                canvas[cy][cx] = 'A'
+                canvas[cy][cx] = 'A '
             if r == exit_p[1] and c == exit_p[0]:
-                canvas[cy][cx] = 'B'
+                canvas[cy][cx] = 'B '
             if v & 1:
                 canvas[cy - 1][cx] = wall
             if v & 2:
@@ -44,7 +45,7 @@ def render_maze(grid: list[list[int]],
             for dy, dx in ((-1, -1), (-1, 1), (1, -1), (1, 1)):
                 canvas[cy + dy][cx + dx] = wall
     for maze_row in canvas:
-        new_row = ' '.join(maze_row)
+        new_row = ''.join(maze_row)
         print(new_row)
     return canvas
 
