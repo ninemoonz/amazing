5534e178415115eb930e031e4f40f8f98e151db0
Ladook <76804681+ninemoonz@users.noreply.github.com>
2026-08-18 18:17:36 +0200

    make Makefile

---
+++ Makefile
@@ -0,0 +1,21 @@
+PYTHON     = python3
+SRC        = a_maze_ing.py
+CONFIG     = config.txt
+OUTPUT     = maze.txt
+
+.PHONY: run clean fclean re mypy
+
+run:
+	$(PYTHON) $(SRC) $(CONFIG)
+
+clean:
+	rm -rf __pycache__
+	rm -rf .mypy_cache
+
+fclean: clean
+	rm -f $(OUTPUT)
+
+re: fclean run
+
+mypy:
+	mypy $(SRC)
---
+++ menu.py
@@ -0,0 +1,16 @@
+while True:
+    print("[1] menu number one\n"
+          "[2] menu number two\n"
+          "[3] menu number three\n"
+          "[4] menu number four\n")
+    choice = input("Choice: ")
+    if choice == "1":
+        print("you choose 1")
+    elif choice == "2":
+        print("you choose 2")
+    elif choice == "3":
+        print("you chooce 3")
+    elif choice == "4":
+        print("you choose 4")
+    else:
+        print("pick between 1-4")
