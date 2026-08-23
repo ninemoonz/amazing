c6ac597b0e6faa7b0d976020412f73932fc5336b
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2026-08-23 19:57:03 +0200

    __init__

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,8 +1,5 @@
 import sys
-from src.parser import parse_config
-from src.validate_and_build import build_config, MazeConfig
-from src.error_class import ConfigError
-from src.menu import menu_func
+from src import parse_config, build_config, MazeConfig, ConfigError, menu_func
 
 
 def main() -> MazeConfig:
---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from src.parser import parse_config
+from src.validate_and_build import build_config, MazeConfig
+from src.error_class import ConfigError
+from src.menu import menu_func
+
